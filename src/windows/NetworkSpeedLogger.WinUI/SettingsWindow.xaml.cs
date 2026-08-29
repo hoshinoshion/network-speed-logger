1d4fda1b7bfb9690b34a7947f34c8274add66847
Shion <27191193+hoshinoshion@users.noreply.github.com>
2026-08-30 01:58:01 +0800

    Fix WinUI compile setup

--- src/windows/NetworkSpeedLogger.WinUI/SettingsWindow.xaml.cs
+++ src/windows/NetworkSpeedLogger.WinUI/SettingsWindow.xaml.cs
@@ -6,6 +6,7 @@ using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 using Windows.Graphics;
 using Windows.Storage;
 using Windows.Storage.Pickers;
