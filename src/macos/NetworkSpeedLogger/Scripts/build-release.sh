552b366aa9f0d0d3e70eea7f53905abc92c6e590
Shion <27191193+hoshinoshion@users.noreply.github.com>
2026-09-11 17:53:29 +0800

    Release macOS v0.6.5

--- src/macos/NetworkSpeedLogger/Scripts/build-release.sh
+++ src/macos/NetworkSpeedLogger/Scripts/build-release.sh
@@ -9,8 +9,8 @@ build_dir="$project_dir/.build/release-universal"
 dist_dir="$repository_root/dist"
 app_path="$dist_dir/NetworkSpeedLogger.app"
 dmg_path="$dist_dir/NetworkSpeedLogger.dmg"
-version=${1:-0.6.4}
-build_number=${2:-20}
+version=${1:-0.6.5}
+build_number=${2:-21}
 
 case "$version" in
     *[!0-9.]*|'')
